9b4997e049e05947c9133f7298a63d211014c5ed
Denis Sitnikov <specden990@mail.ru>
2023-11-23 16:50:12 +0500

    Добавлено получение данных из localStorage

--- task_5.js
+++ task_5.js
@@ -4,38 +4,44 @@ const button = document.querySelector('button'),
 
 // Если есть необходимость проверить работу ы браузере, то необходимо подключить скрипт а файле index.html
 
-    function renderImage(data){
-        const urlArr = data.map(obj => obj.url);
-        container.textContent = ''; 
-        urlArr.forEach(url => {
-            const image = document.createElement('img');
-            image.style.width = '200px';
-            image.src = url;
-            container.appendChild(image); 
-        });
-    };  
+const requestToTheServer = (page, limit) =>{
+    return fetch(`https://jsonplaceholder.typicode.com/photos?_page=${page}&_limit=${limit}`)
+            .then((response) =>{ return response.json()})
+            .then((data) =>{return renderImage(data)})
+            .catch(() => container.textContent = `Не удалось связаться с сервером`);
+}
+  
+requestToTheServer(localStorage.getItem('page'), localStorage.getItem('limit'))
 
-    const requestToTheServer = (page, limit) =>{
-        return fetch(`https://jsonplaceholder.typicode.com/photos?_page=${page}&_limit=${limit}`)
-                .then((response) =>{ return response.json()})
-                .then((data) =>{return renderImage(data)})
-                .catch((error) => container.textContent = `Не удалось связаться с сервером`);
-    }
-      
-    button.addEventListener('click', async (e) =>{
-        e.preventDefault();
+function renderImage(data){
+    const urlArr = data.map(obj => obj.url);
+    container.textContent = ''; 
+    urlArr.forEach(url => {
+        const image = document.createElement('img');
+        image.style.width = '200px';
+        image.src = url;
+        container.appendChild(image); 
+    });
+};  
+
+button.addEventListener('click', async (e) =>{
+    e.preventDefault();
         let page = +inputs[0].value,
             limit = +inputs[1].value;
 
-        if((typeof(page) !== 'number' ||  isNaN(page) || page < 1 || page > 10) &&
-             (typeof(limit) !== 'number' || isNaN(limit) || limit < 1 || limit > 10)){
-            container.textContent = 'Номер страницы и лимит вне диапазона от 1 до 10';
-        }else if(typeof(page) !== 'number' ||  isNaN(page) || page < 1 || page > 10){
-            container.textContent = `Номер страницы вне диапазона от 1 до 10`;
-        }else if(typeof(limit) !== 'number' || isNaN(limit) || limit < 1 || limit > 10){
-            container.textContent = `Лимит вне диапазона от 1 до 10`;
-        }
-        else{
-            await requestToTheServer(page, limit);
-        }
-    });  
+        localStorage.setItem('page', +inputs[0].value);
+        localStorage.setItem('limit', +inputs[1].value);
+
+
+    if((typeof(page) !== 'number' ||  isNaN(page) || page < 1 || page > 10) &&
+            (typeof(limit) !== 'number' || isNaN(limit) || limit < 1 || limit > 10)){
+        container.textContent = 'Номер страницы и лимит вне диапазона от 1 до 10';
+    }else if(typeof(page) !== 'number' ||  isNaN(page) || page < 1 || page > 10){
+        container.textContent = `Номер страницы вне диапазона от 1 до 10`;
+    }else if(typeof(limit) !== 'number' || isNaN(limit) || limit < 1 || limit > 10){
+        container.textContent = `Лимит вне диапазона от 1 до 10`;
+    }
+    else{
+        await requestToTheServer(page, limit);
+    }
+});  
